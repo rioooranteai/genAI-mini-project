c69250ddd9204855098388856d477f5cd4ec9787
Mario Valerian Rante Ta'dung <rioooranteai@gmail.com>
2025-01-29 22:48:20 +0800

    feat: finish langchain_helper code and add placement for text

--- Mission: Impossible to Work/langchain_helper.py
+++ Mission: Impossible to Work/langchain_helper.py
@@ -1,22 +1,47 @@
 import os
-import openai
-
 from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
+# Load API Key
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+def text_generator(media_type: str, job: str, category: str, temperature: float):
+    temperature = temperature / 10
 
-def text_generator(media_type:str, job, category):
+    # Inisialisasi model LLM dengan API Key langsung
+    llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), temperature=temperature)
+
+    # Template untuk pesan
+    prompt_template_message = PromptTemplate(
+        input_variables=["job", "category", "media_type"],
+        template="""Pagi ini rasanya waktu yang sempurna untuk lanjut tidur.
+        Namun, saya perlu memberikan alasan yang masuk akal untuk izin tidak masuk kerja.
+        Buatkan saya alasan yang lucu dan absurd, seolah-olah saya seorang komedian legendaris
+        yang bisa membuat atasan yang paling serius pun tersenyum. Ingat, posisi saya adalah {job},
+        jadi alasan yang diberikan harus relevan dan membahas tentang {category}, tapi tetap menghibur!
+        Alasan ini akan dikirimkan melalui {media_type}."""
+    )
+
+    message_chain = LLMChain(llm=llm, prompt=prompt_template_message, output_key="message")
+
+
     if media_type == "Email":
-        llm = OpenAI(temperature=0.7)
-
         prompt_template_subject = PromptTemplate(
-            input_variables=['cuisine'],
-            template="I want to open a restaurant for {cuisine} food. Suggest a fency name for this."
+            input_variables=["job"],
+            template="Buat subject email untuk pesan izin tidak masuk kantor untuk departemen {job}"
         )
 
-        subject_chain = LLMChain(llm=llm, prompt=prompt_template_subject, output_key="restaurant_name")
+        subject_chain = LLMChain(llm=llm, prompt=prompt_template_subject, output_key="email_subject")
+
+        # Eksekusi kedua LLMChain secara independen
+        subject = subject_chain.run({"job": job})
+        message = message_chain.run({"job": job, "category": category, "media_type": media_type})
+
+        return {"email_subject": subject, "message": message}
+
+    else:
+        # Untuk Chat WA, Tele, dan App pesan  chat lainnya
+        message = message_chain.run({"job": job, "category": category, "media_type": media_type})
+        return {"message": message}
---
+++ Mission: Impossible to Work/main.py
@@ -0,0 +1,69 @@
+import streamlit as st
+
+import langchain_helper
+
+st.title("Generate Your Absurdly Excused")
+
+# Daftar awal untuk pekerjaan dan kategori
+default_jobs = ["Data Scientist", "Software Engineer", "Product Manager", "Policeman", "Teacher"]
+default_cat = ["Animal", "Family", "Vehicle"]
+default_media = ['Chat Apps', 'Email']
+
+# Sidebar: Job Picker
+st.sidebar.title("Job Picker")
+job = st.sidebar.selectbox("Pick A Job", default_jobs)
+
+# Input tambahan untuk pekerjaan
+new_job = st.sidebar.text_input("Add a new job if not listed", key="job_input")
+if st.sidebar.button("Add Job"):
+    if new_job and new_job not in default_jobs:
+        default_jobs.append(new_job)
+        job = new_job
+    elif not new_job:
+        st.sidebar.error("Please enter a job to add!")
+    else:
+        st.sidebar.error("Job already exists!")
+
+# Sidebar: Category Picker
+st.sidebar.title("Category Picker")
+category = st.sidebar.selectbox("Pick A Category", default_cat)
+
+# Input tambahan untuk kategori
+new_category = st.sidebar.text_input("Add a new category if not listed", key="category_input")
+if st.sidebar.button("Add Category"):
+    if new_category and new_category not in default_cat:
+        default_cat.append(new_category)
+        category = new_category
+    elif not new_category:
+        st.sidebar.error("Please enter a category to add!")
+    else:
+        st.sidebar.error("Category already exists!")
+
+# Sidebar: Media Picker
+st.sidebar.title("Media Picker")
+media = st.sidebar.selectbox("Pick A Media", default_media)
+
+# Sidebar: OpenAI Temperature
+st.sidebar.title("OpenAI Temperature")
+
+slider_helper_text = """
+The LLM temperature in OpenAI refers to a parameter that controls the randomness of the model's responses, 
+where higher values (e.g., 1.0) make the output more creative and diverse, while lower values (e.g., 0.2) make it more 
+focused and deterministic.
+"""
+
+temperature = st.sidebar.slider("Set Your OpenAI Temperature", 0, 10, 5,  help=slider_helper_text)
+
+if st.sidebar.button("Generate Excused", key="sidebar_button"):
+    response = langchain_helper.text_generator(media, job, category, temperature)
+
+    if media == "Email":
+        subject = response['email_subject']
+        message = response['message']
+        st.write(f"Subject : {subject}")
+        st.write(message)
+
+    else:
+        message = response['message']
+        st.write(message)
+
